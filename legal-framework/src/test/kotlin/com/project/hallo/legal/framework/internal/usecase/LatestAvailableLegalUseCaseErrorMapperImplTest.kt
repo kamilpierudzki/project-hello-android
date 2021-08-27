1246b6267f29fba67ce4f4f24f9186065063ebf4
kamil.pierudzki <kamil.pierudzki@outlook.com>
2021-08-27 12:05:49 +0200

    Create unit test for LatestAvailableLegalUseCaseErrorMapperImpl.kt

---
+++ legal-framework/src/test/kotlin/com/project/hallo/legal/framework/internal/usecase/LatestAvailableLegalUseCaseErrorMapperImplTest.kt
@@ -0,0 +1,46 @@
+package com.project.hallo.legal.framework.internal.usecase
+
+import android.content.res.Resources
+import com.project.hallo.commons.domain.data.Response
+import com.project.hello.legal.domain.model.LatestAvailableLegal
+import com.project.hello.legal.domain.model.LegalErrorCode.LatestAvailableLegal.LATEST_AVAILABLE_LEGAL_ERROR
+import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+
+internal class LatestAvailableLegalUseCaseErrorMapperImplTest {
+
+    val translatedString = "translated"
+    val resources: Resources = mock {
+        on { getString(any()) } doReturn translatedString
+    }
+
+    val tested = LatestAvailableLegalUseCaseErrorMapperImpl(resources)
+
+    @Test
+    fun `given raw error is LATEST_AVAILABLE_LEGAL_ERROR when mapError is called then error is translated`() {
+        // given
+        val error = Response.Error<LatestAvailableLegal>(LATEST_AVAILABLE_LEGAL_ERROR)
+
+        // when
+        tested.mapError(error)
+
+        // then
+        assert(error.localisedErrorMessage == translatedString)
+    }
+
+    @Test
+    fun `given raw error is different than LATEST_AVAILABLE_LEGAL_ERROR when mapError is called then error is NOT translated`() {
+        // given
+        val rawError = "rawError"
+        val error = Response.Error<LatestAvailableLegal>(rawError)
+
+        // when
+        tested.mapError(error)
+
+        // then
+        assert(error.localisedErrorMessage != translatedString)
+        assert(error.localisedErrorMessage == rawError)
+    }
+}
