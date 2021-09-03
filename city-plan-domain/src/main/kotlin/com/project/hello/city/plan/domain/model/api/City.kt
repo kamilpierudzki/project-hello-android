98d5af552cfd0953fbfa3f497d134e54bd003d2a
kamil.pierudzki <kamil.pierudzki@outlook.com>
2021-09-03 16:12:31 +0200

    Add support for Krakow

--- city-plan-domain/src/main/kotlin/com/project/hello/city/plan/domain/model/api/City.kt
+++ city-plan-domain/src/main/kotlin/com/project/hello/city/plan/domain/model/api/City.kt
@@ -3,6 +3,7 @@ package com.project.hello.city.plan.domain.model.api
 import com.project.hello.city.plan.domain.R
 
 internal enum class City(val file: Int) {
+    KRAKOW(R.raw.krakow),
     POZNAN(R.raw.poznan),
     WARSZAWA(R.raw.warszawa)
 }
