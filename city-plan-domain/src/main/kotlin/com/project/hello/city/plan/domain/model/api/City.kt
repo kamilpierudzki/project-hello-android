19d8444f9e7721fe46739ced2861d3b03a3b971b
kamil.pierudzki <kamil.pierudzki@outlook.com>
2021-09-13 22:30:48 +0200

    Add support for Wroclaw

--- city-plan-domain/src/main/kotlin/com/project/hello/city/plan/domain/model/api/City.kt
+++ city-plan-domain/src/main/kotlin/com/project/hello/city/plan/domain/model/api/City.kt
@@ -3,7 +3,8 @@ package com.project.hello.city.plan.domain.model.api
 import com.project.hello.city.plan.domain.R
 
 internal enum class City(val file: Int) {
-    KRAKOW(R.raw.krakow),
     POZNAN(R.raw.poznan),
-    WARSZAWA(R.raw.warszawa)
+    KRAKOW(R.raw.krakow),
+    WARSZAWA(R.raw.warszawa),
+    WROCLAW(R.raw.wroclaw)
 }
