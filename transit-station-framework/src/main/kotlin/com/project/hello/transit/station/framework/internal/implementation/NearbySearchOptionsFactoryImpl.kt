f956b090913079afd5809db8156aaf6b89fe9ce3
kamil.pierudzki <kamil.pierudzki@outlook.com>
2021-10-08 00:16:25 +0200

    Remove hardcoded location

--- transit-station-framework/src/main/kotlin/com/project/hello/transit/station/framework/internal/implementation/NearbySearchOptionsFactoryImpl.kt
+++ transit-station-framework/src/main/kotlin/com/project/hello/transit/station/framework/internal/implementation/NearbySearchOptionsFactoryImpl.kt
@@ -10,7 +10,7 @@ import javax.inject.Inject
 
 internal class NearbySearchOptionsFactoryImpl @Inject constructor() : NearbySearchOptionsFactory {
     override fun create(location: Location) = mapOf(
-        ParamName.LOCATION_NAME to /*"${location.latitude},${location.longitude}",*/"52.394737,16.949834",
+        ParamName.LOCATION_NAME to "${location.latitude},${location.longitude}",
         ParamName.RADIUS_NAME to ParamValue.RADIUS_VALUE,
         ParamName.PLACE_TYPE_NAME to ParamValue.PLACE_TYPE_VALUE,
         ParamName.KEY_NAME to BuildConfig.GOOGLE_MAPS_API_KEY
