70cc02cd73daec0abbfa488230aaf98effc88f04
Kamil Pierudzki <kamil.pierudzki@outlook.com>
2022-02-21 15:11:55 +0100

    Update the app version

--- buildSrc/src/main/java/com/project/hello/script/Config.kt
+++ buildSrc/src/main/java/com/project/hello/script/Config.kt
@@ -4,6 +4,6 @@ object Config {
     const val compileSdk = 31
     const val minSdk = 23
     const val targetSdk = 31
-    const val versionCode = 28
-    const val versionName = "1.7.0"
+    const val versionCode = 29
+    const val versionName = "1.7.1"
 }
